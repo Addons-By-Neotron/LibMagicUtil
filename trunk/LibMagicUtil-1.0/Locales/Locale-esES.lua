bc81c404afa12a15a1f829a89c912abe3735a41a
dhedbor <dhedbor@f180bc82-bf1c-4033-9456-9c7259c86c06>
2009-04-12 07:25:58 +0000

    Initial checking: Utility library for NeoTron's addons.

---
+++ trunk/LibMagicUtil-1.0/Locales/Locale-esES.lua
@@ -0,0 +1,9 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LibMagicUtil-1.0", "esES")
+if not L then return end
+
+-- To help localize LibMagicUtil-1.0 please enter phrase translations on the
+-- following URL:
+-- http://www.wowace.com/projects/libmagicutil-10/localization/
+-- This file should not be edited manually!
+
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="handle-subnamespaces")@
