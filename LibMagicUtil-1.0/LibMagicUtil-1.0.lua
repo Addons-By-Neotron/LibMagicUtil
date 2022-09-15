dd55c61305e5d91731daaab617d4d8f56b29ff55
David Hedbor <neotron@gmail.com>
2022-09-15 14:50:33 +0200

    Add dump method.

--- LibMagicUtil-1.0/LibMagicUtil-1.0.lua
+++ LibMagicUtil-1.0/LibMagicUtil-1.0.lua
@@ -165,6 +165,19 @@ function lib:_SetBackgroundOption(...)
    self:FixBackdrop()
 end
 
+function lib:Dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k,v in pairs(o) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            s = s .. '['..k..'] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
 -- Config template for a frame background
 optionTemplates.background = {
    type = "group",
