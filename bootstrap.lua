87b1799b613d717e3feafe294f27ac1b49a9ee06
David Hedbor <neotron@gmail.com>
2026-03-07 21:55:41 +0100

    Add dependencies and missing bootstrap file.

---
+++ bootstrap.lua
@@ -0,0 +1,3 @@
+-- Flag that we're loading from the standalone addon, not an embedded copy.
+-- This allows the lib to bump its MINOR version so the standalone always wins.
+LIBMAGICUTIL_STANDALONE = true
